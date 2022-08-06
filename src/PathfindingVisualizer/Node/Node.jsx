7931a5eccb452b774c94aa45e940b820305d1ca4
Manny <ialmosara@gmail.com>
2022-08-06 18:33:08 -0400

    finished

--- src/PathfindingVisualizer/Node/Node.jsx
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, {Component} from 'react';
+
 import './Node.css';
 
-const Node = () => {
-  return <div
-    className='node'
-  ></div>;
-};
+export default class Node extends Component {
+  render() {
+    const {
+      col,
+      isFinish,
+      isStart,
+      isWall,
+      onMouseDown,
+      onMouseEnter,
+      onMouseUp,
+      row,
+    } = this.props;
+    const extraClassName = isFinish
+      ? 'node-finish'
+      : isStart
+      ? 'node-start'
+      : isWall
+      ? 'node-wall'
+      : '';
 
-export default Node;
+    return (
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+        onMouseDown={() => onMouseDown(row, col)}
+        onMouseEnter={() => onMouseEnter(row, col)}
+        onMouseUp={() => onMouseUp()}></div>
+    );
+  }
+}
